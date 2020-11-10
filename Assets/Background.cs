098b13dc260f9e3a1167722494a0a6db7a56d7b2
tiitustelke <tiitus.telke@metropolia.fi>
2020-11-10 16:46:21 +0200

    Add moving background

---
+++ Assets/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Thanks MadFireOn https://www.youtube.com/watch?v=32EIYs6Z18Q
+public class Background : MonoBehaviour
+{
+    public float backGroundSpeed;
+    public Renderer backgroundRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundRend.material.mainTextureOffset += new Vector2(backGroundSpeed * Time.deltaTime, 0f);
+    }
+}
