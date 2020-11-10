8eb6343dfedf6ac0337affb0eae80b9bc8afc7fd
mikaelch <mikael.holm@metropolia.fi>
2020-11-10 07:29:23 +0200

    Add missing files

---
+++ Assets/PlayerInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo
+{
+    public static string name { get; set; }
+    public static int score { get; set; }
+    public static int level { get; set; }
+}
---
+++ Assets/PlayerWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Enemy>().DecreaseHealth();
+            PlayerInfo.score++;
+            Destroy(gameObject);
+        }
+    }
+}
