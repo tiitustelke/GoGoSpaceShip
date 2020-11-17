37a5ac8ba479c0a43e5b889c92fdabad3b88f4da
aleksikyto <aleksi.kyto@hotmail.com>
2020-11-17 13:57:48 +0200

    Local files

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    Button playButton;
+    Button quitButton;
+    Button settingsButton;
+    Button scoreButton;
+
+    MainMenu mainMenu;
+    SettingsMenu settingsMenu;
+    ScoresMenu scoresMenu;
+
+    void Start()
+    {
+        playButton = (Button)GameObject.Find("PlayButton").GetComponent<Button>();
+        quitButton = (Button)GameObject.Find("QuitButton").GetComponent<Button>();
+        settingsButton = (Button)GameObject.Find("SettingsButton").GetComponent<Button>();
+        scoreButton = (Button)GameObject.Find("ScoreButton").GetComponent<Button>();
+
+        settingsMenu = GameObject.Find("SettingsMenu").GetComponent<SettingsMenu>();
+        mainMenu = GameObject.Find("MainMenu").GetComponent<MainMenu>();
+        scoresMenu = GameObject.Find("ScoresMenu").GetComponent<ScoresMenu>();
+
+        settingsMenu.gameObject.SetActive(false);
+        scoresMenu.gameObject.SetActive(false);
+
+        playButton.onClick.AddListener(() => PlayGame());
+        quitButton.onClick.AddListener(() => QuitGame());
+
+        settingsButton.onClick.AddListener(() => Settings());
+        scoreButton.onClick.AddListener(() => Scores());
+
+    }
+
+
+    public void PlayGame()
+    {
+        Debug.Log("Starting Game....");
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit.");
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings...");
+        mainMenu.gameObject.SetActive(false);
+        settingsMenu.gameObject.SetActive(true);
+    }
+
+    public void Scores()
+    {
+        Debug.Log("Scores....");
+        mainMenu.gameObject.SetActive(false);
+        scoresMenu.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public PauseMenu pauseMenu;
+
+    /*BUTTONIN PUUTTUU VIELÄ*/
+    /*Toimii nyt vaan kun unitysta pause menulle valittu pausemenu*/
+
+    void Start()
+    {
+        pauseMenu = GameObject.Find("PauseMenu").GetComponent<PauseMenu>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Debug.Log("Click");
+                Resume();
+            }
+            else
+            {
+                Debug.Log("Click");
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
