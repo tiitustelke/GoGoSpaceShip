e900c31ecfea84e0c4f3ff22ca1e613e49ec5274
Tiitus Telke <tiitus.telke@metropolia.fi>
2020-12-01 16:20:12 +0200

    Fix reference error

--- Assets/StorySubtitles.cs
+++ Assets/StorySubtitles.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class StorySubtitles : MonoBehaviour
 {
     //pitää tehdä boolean jolla saa asetuksista tekstitykset pois.
-    public GameObject textBox;
+    private Text textBox;
     // Start is called before the first frame update
     void Start()
     {
+        textBox = GameObject.Find("StoryText").GetComponent<Text>();
         if (PlayerInfo.story)
         {
             playIntro();
@@ -31,19 +32,19 @@ public class StorySubtitles : MonoBehaviour
     {
         //FindObjectOfType<AudioManager>().Play("Intro");
         yield return new WaitForSeconds(1);
-        textBox.GetComponent<Text>().text = "Pilot, you are doing a great job.";
+        textBox.text = "Pilot, you are doing a great job.";
         yield return new WaitForSeconds(2);
-        textBox.GetComponent<Text>().text = "With this pace you will reach planet Elon by tommorow noon.";
+        textBox.text = "With this pace you will reach planet Elon by tommorow noon.";
         yield return new WaitForSeconds(2);
-        textBox.GetComponent<Text>().text = "We won't let anyone stop you from delivering the artifact in time.";
+        textBox.text = "We won't let anyone stop you from delivering the artifact in time.";
         yield return new WaitForSeconds(2);
-        textBox.GetComponent<Text>().text = "";
+        textBox.text = "";
         yield return new WaitForSeconds(2);
-        textBox.GetComponent<Text>().text = "Oh, oh! We detected an anomaly on the radar";
+        textBox.text = "Oh, oh! We detected an anomaly on the radar";
         yield return new WaitForSeconds(3);
-        textBox.GetComponent<Text>().text = "Hostiles approaching! Prepare your lasergun";
+        textBox.text = "Hostiles approaching! Prepare your lasergun";
         yield return new WaitForSeconds(2);
-        textBox.GetComponent<Text>().text = "";
+        textBox.text = "";
         // FindObjectOfType<AudioManager>().Stop("Intro");
         PlayerInfo.story = false;
         yield break;
